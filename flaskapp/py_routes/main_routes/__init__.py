d15e3aa767526330b8710ca043143bc4672fd606
Anen0 <Lum3nGee@gmail.com>
2026-01-19 15:08:42 +0800

    Pages ready

--- flaskapp/py_routes/main_routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# main = Blueprint('main', __name__, template_folder='folder-name')
-main = Blueprint('main', __name__)
-
-from . import general_routes, login_routes
-
-
-
--- wsgi.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from flaskapp import run_dammit
-
-app = run_dammit()
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=6780, debug=True)
